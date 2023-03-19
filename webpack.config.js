f9251c963dd54515c1b1eb82488164532b7298c0
Puzakov Maxym <m.puzakov90@gmail.com>
2023-03-19 22:45:55 +0200

    add webpack proj

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const postCssPresetENV = require('postcss-preset-env');
 
 const mode = process.env.NODE_ENV || 'development';
 const devMode = mode == 'development';
@@ -11,24 +13,46 @@ module.exports = {
     mode,
     target,
     devtool,
+    devServer: {
+      port: 3000,
+      open: true,
+      hot: true,
+    },
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
         filename: 'index.[contenthash].js',
     },
-    plugins: [new HtmlWebpackPlugin({
+    plugins: [
+      new HtmlWebpackPlugin({
         template: path.resolve(__dirname, 'src', 'index.html'),
-    })],
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+      })
+    ],
     module: {
         rules: [
           {
             test: /\.html$/i,
-            loader: "html-loader",
+            loader: 'html-loader',
           },
           {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
+            test: /\.(c|sa|sc)ss$/i,
+            use: [
+              devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  postcssOptions: {
+                    plugin: [postCssPresetENV]
+                  },
+                },
+              },
+              'sass-loader',
+            ],
           },
         ],
       },
